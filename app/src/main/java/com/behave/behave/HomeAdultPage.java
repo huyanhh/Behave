cdd2e6088b895c8ab86fb7a656d8fa5f1cc05508
Huyanh Hoang <huyanhh@uci.edu>
2017-02-06 20:06:52 -0800

    add prize list

---
+++ app/src/main/java/com/behave/behave/HomeAdultPage.java
@@ -0,0 +1,16 @@
+package com.behave.behave;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huyanh on 2017. 2. 6..
+ */
+
+public class HomeAdultPage extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_adult_page);
+
+    }
+}
